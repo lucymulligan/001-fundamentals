a08f5ca88534ec2a961fa03c36296cd91b2a11ef
Lucy Mulligan <lucymulligan@hotmail.com>
2019-04-02 21:07:22 +0100

    Week 1 Progress

--- app/calculator.js
+++ app/calculator.js
@@ -3,5 +3,34 @@ function add(a, b) {
 }
 
 module.exports = {
-    add
-};
+    add, 
+    subtract,
+    multiply,
+    isOdd,
+    isEven,
+    isDivisibleBy
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function isOdd(value) {
+    if (value%2 == 0)
+    return false;
+    else
+    return true;
+ }
+ function isEven(value) {
+    if (value%2 == 0)
+    return true;
+    else 
+    return false;
+ }
+ function isDivisibleBy(value) {
+     if (value%2 == 0) 
+     return true;
+     else 
+     return false;
+ }
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,19 @@ const calculator = require('../app/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+test('subtracts 3 - 2 to equal 1', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+test('multiplies 3 * 2 to equal 6', () => {
+  expect(calculator.multiply(3, 2)).toBe(6);
+});
+test('isOdd = 5', () => {
+  expect(calculator.isOdd(5)).toBe(true);
+ });
+ test('isEven = 4', () => {
+  expect(calculator.isEven(4)).toBe(true);
+ });
+ test('isDivisibleBy = 2', () => {
+  expect(calculator.isDivisibleBy(10, 2)).toBe(true); 
+  expect(calculator.isDivisibleBy(13, 7)).toBe(false);
+ });
